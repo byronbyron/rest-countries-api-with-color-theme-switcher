3c63a6247db9e72f14d6adbc95e7a11e412d60c8
Byronn Walker-Mills <bwalkermills@gmail.com>
2021-12-10 13:05:30 +0000

    Add <CountryItem> component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import { Link } from 'react-router-dom'
+import CountryItem from '../components/CountryItem'
 
 export default function Home() {
   const [countries, setCountries] = useState([])
@@ -22,13 +23,13 @@ export default function Home() {
       <div className="container mx-auto pb-24">
         <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16 px-14 sm:px-4">
           { countries.map( (country, index) => <Link to={{ pathname: 'details', state: country }} key={index}>
-            <div>
-              <img src={country.flag} alt={country.name} />
-              <p><strong>{country.name}</strong></p>
-              <p>{country.population}</p>
-              <p>{country.region}</p>
-              <p>{country.capital}</p>
-            </div>
+            <CountryItem
+              title={country.name}
+              image_url={country.flag}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+            />
           </Link>) }
         </div>
       </div>
