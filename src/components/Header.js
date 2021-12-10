5a32e38b6e0b6dbd9f209bf1ac392053c6df987d
Byronn Walker-Mills <bwalkermills@gmail.com>
2021-12-10 12:56:11 +0000

    Pull out country list data, update font

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,8 +22,8 @@ export default function Header() {
         </h1>
 
         <div className="ml-auto font-medium">
-          <button onClick={() => toggleDarkMode()}>
-            <i className="far fa-moon" aria-hidden="true"></i><span className="font-semibold ml-2">Dark Mode</span>
+          <button className="flex items-center" onClick={() => toggleDarkMode()}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="inline-block w-5 h-5" viewBox="0 0 512 512"><path d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/></svg><span className="font-semibold ml-2">Dark Mode</span>
           </button>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Nunito Sans', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
