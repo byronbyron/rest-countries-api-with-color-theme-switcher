c7b719537e3c9aed616af2daec40046156eb6bca
Byronn Walker-Mills <bwalkermills@gmail.com>
2021-12-10 13:15:59 +0000

    Add country search and filter to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,11 +16,50 @@ export default function Home() {
     await setCountries(data)
   }
 
+  const searchCountry = async (term) => {
+    if (term === '') {
+      await getCountries()
+      return
+    }
+
+    const res = await fetch(`https://restcountries.com/v2/name/${term}`)
+    const data = await res.json()
+
+    if (data.status === 404) return
+
+    await setCountries(data)
+  }
+
+  const filterByRegion = async (region) => {
+    if (region === '') return
+    const res = await fetch(`https://restcountries.com/v2/region/${region}`)
+    const data = await res.json()
+    await setCountries(data)
+  }
+
   return (
     <div className="bg-gray-50 dark:bg-gray-800 dark:text-white">
       <Header />
 
       <div className="container mx-auto pb-24">
+        <div className="flex flex-col sm:flex-row mb-8 sm:mb-12 px-4">
+          <div className="relative flex w-full sm:w-1/2 md:w-1/3 flex-wrap items-stretch mb-10 sm:mb-0 sm:mr-auto">
+            <span className="absolute h-full flex items-center pl-8">
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500" viewBox="0 0 512 512" fill="currentColor"><path d="M464 428L339.92 303.9a160.48 160.48 0 0030.72-94.58C370.64 120.37 298.27 48 209.32 48S48 120.37 48 209.32s72.37 161.32 161.32 161.32a160.48 160.48 0 0094.58-30.72L428 464zM209.32 319.69a110.38 110.38 0 11110.37-110.37 110.5 110.5 0 01-110.37 110.37z"/></svg>
+            </span>
+            <input type="text" placeholder="Search for a country..." className="text-sm pl-18 p-3 sm:py-4 shadow-md rounded-md w-full border-0 dark:bg-gray-700 dark:placeholder-gray-300 dark:placeholder-opacity-100" onChange={(term) => searchCountry(term.target.value)} />
+          </div>
+
+          <select name="" id="" className="form-select w-48 py-3 px-4 sm:py-4 sm:px-6 shadow-md rounded-md border-0 dark:bg-gray-700" onChange={(val) => filterByRegion(val.target.value)}>
+            <option value="">Filter by Region</option>
+            <option value="africa">Africa</option>
+            <option value="americas">Americas</option>
+            <option value="asia">Asia</option>
+            <option value="europe">Europe</option>
+            <option value="oceania">Oceania</option>
+          </select>
+        </div>
+
         <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16 px-14 sm:px-4">
           { countries.map( (country, index) => <Link to={{ pathname: 'details', state: country }} key={index}>
             <CountryItem
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
     fontFamily: {
       'sans': ['Nunito Sans', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      padding: {
+        '18': '4.5rem',
+      },
+      boxShadow: {
+        sm: '0 0 0.25rem rgba(0, 0, 0, 0.2)',
+        md: '0 0 0.5rem rgba(0, 0, 0, 0.05)',
+        lg: '0 0 0.5rem rgba(0, 0, 0, 0.3)',
+      },
+      container: {
+        screens: {
+          'xl': '1312px'
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
