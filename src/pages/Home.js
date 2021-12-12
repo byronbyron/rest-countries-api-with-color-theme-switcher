1fee7fdcb49d21e46bc5718946c06a4e148d4c65
Byronn Walker-Mills <bwalkermills@gmail.com>
2021-12-12 17:25:27 +0000

    Update search input type to type="search"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -48,7 +48,7 @@ export default function Home() {
               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-gray-500" viewBox="0 0 512 512" fill="currentColor"><path d="M464 428L339.92 303.9a160.48 160.48 0 0030.72-94.58C370.64 120.37 298.27 48 209.32 48S48 120.37 48 209.32s72.37 161.32 161.32 161.32a160.48 160.48 0 0094.58-30.72L428 464zM209.32 319.69a110.38 110.38 0 11110.37-110.37 110.5 110.5 0 01-110.37 110.37z"/></svg>
             </span>
             <label className="sr-only" for="search">Search for a country</label>
-            <input type="text" id="search" placeholder="Search for a country..." className="text-sm pl-18 p-3 sm:py-4 shadow-md rounded-md w-full border-0 dark:bg-gray-700 dark:placeholder-gray-300 dark:placeholder-opacity-100" onChange={(term) => searchCountry(term.target.value)} />
+            <input type="search" id="search" placeholder="Search for a country..." className="text-sm pl-18 p-3 sm:py-4 shadow-md rounded-md w-full border-0 dark:bg-gray-700 dark:placeholder-gray-300 dark:placeholder-opacity-100" onChange={(term) => searchCountry(term.target.value)} />
           </div>
 
           <label for="filter" className="sr-only">Filter by Region</label>
